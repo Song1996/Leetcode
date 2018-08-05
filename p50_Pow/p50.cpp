d0b2c915ffc87a30cf5ff49ca1cbecc0f2267d48
Song Zhang <141242045@smail.nju.edu.cn>
2018-08-05 16:32:26 +0800

    finished p50 pow

---
+++ p50_Pow/p50.cpp
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        B = 0x80000000;
+        if (n==0) {
+            return 1.0;
+        } 
+        bool nflag = (n<0);
+        n = abs(n);
+        int pos = 31;
+        while (!(B&n)) {
+            pos --;
+            B >>= 1;
+        }
+        saved_pow[0] = x;
+        for (int i = 1; i <= pos; i++) {
+            saved_pow[i] = saved_pow[i-1]*saved_pow[i-1];
+        }
+        double result = 1.0;
+        while (B) {
+            if (B&n) {
+                result *= saved_pow[pos];
+            }
+            pos --;
+            B >>= 1;
+        }
+        if (nflag) {
+            result = 1/result;
+        }
+        return result;
+    }
+private:
+    double saved_pow[33];
+    unsigned int B;
+};
+
+int main() {
+    class Solution s = Solution();
+    printf("answer %f\n",s.myPow(2.0,10));
+    printf("answer %f\n",s.myPow(2.0,-2));
+    printf("answer %f\n",s.myPow(2.10000, 3));
+}
