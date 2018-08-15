da255db114d9b6a557555506f2744c02f4517cbd
Song Zhang <greatzhangs@outlook.com>
2018-08-15 10:29:32 +0800

    finished p94 with stack

---
+++ p94_Binary_Tree_Inorder_Traversal/p94.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <stack>
+#include <map>
+#include <string>
+#include <assert.h>
+#include <algorithm>
+using namespace std;
+
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if(root==NULL) return ans;
+        stack<TreeNode*> s;
+        s.push(root);
+        while(s.size()>0) {
+            TreeNode* t = s.top();
+            if(t->left!=NULL) {
+                s.push(t->left);
+                t->left=NULL;
+            }else {
+                s.pop();
+                ans.push_back(t->val);
+                if(t->right!=NULL) s.push(t->right);
+            }
+        }
+        return ans;
+    }
+};
+
+int main () {
+    TreeNode* x = new TreeNode(1);
+    x->right = new TreeNode(2);
+    x->right->left = new TreeNode(3);
+    Solution s;
+    vector<int> y = s.inorderTraversal(x);
+    for(int i = 0; i<y.size(); i++) {
+        printf("%d ",y[i]);
+    }printf("\n");
+	return 0;
+}
