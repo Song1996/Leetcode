c98af8372c79d55290c4821073dea0449bd0d7da
Song Zhang <greatzhangs@outlook.com>
2018-08-27 23:40:01 +0800

    finished p217

---
+++ p217_Contains_Duplicate/p217.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <map>
+#include <string>
+#include <assert.h>
+#include <stdlib.h>
+#include <fstream>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        for(int i = 1; i < nums.size(); i++) if(nums[i]==nums[i-1]) return true;
+        return false;
+    }
+};
+
+int main () {
+	return 0;
+}
