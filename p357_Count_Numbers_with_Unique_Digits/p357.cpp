b0c32d003a0ef1ce6111d11939e596a7387faac1
Song Zhang <greatzhangs@outlook.com>
2018-08-28 18:11:58 +0800

    finished p357 with dp

---
+++ p357_Count_Numbers_with_Unique_Digits/p357.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <map>
+#include <string>
+#include <assert.h>
+#include <stdlib.h>
+#include <fstream>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int countNumbersWithUniqueDigits(int n) {
+        if(n>10) return 0;
+        if(n == 0) return 1;
+        if(n == 1) return 10;
+        vector<int> mem(11, -1);
+        mem[1] = 10;
+        int ans = 10;
+        for(int i = 2; i <= n; i++) {
+            printf("%d\n",ans);
+            ans += (dp(i, mem)/10)*9;
+        }
+        return ans;
+    }
+    int dp(int n, vector<int>& mem) {
+        if(mem[n]>0) return mem[n];
+        mem[n] = (11-n)*dp(n-1, mem);
+        printf("%d %d\n",n,mem[n]);
+        return mem[n];
+    }
+};
+
+int main () {
+    Solution s;
+    printf("%d\n",s.countNumbersWithUniqueDigits(3));
+	return 0;
+}
