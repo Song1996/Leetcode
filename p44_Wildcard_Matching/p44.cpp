ad86c02192b7e2ad6cbc95bd88210ec047f50d63
Song Zhang <greatzhangs@outlook.com>
2018-08-28 20:37:57 +0800

    update p44 with a little improvement

--- p44_Wildcard_Matching/p44.cpp
+++ p44_Wildcard_Matching/p44.cpp
@@ -14,7 +14,9 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int> > dict;
-    bool isMatch(string s, string p) {
+    string s; string p;
+    bool isMatch(string xs, string xp) {
+        s = xs; p = xp;
         dict.clear(); dict.resize(s.length()+1);
         for(int i = 0; i < s.length()+1; i++) dict[i].resize(p.length()+1);
         /*printf("%s %s\n",s.c_str(),p.c_str());
@@ -24,26 +26,26 @@ public:
             printf("\n");
         }
         printf("entered\n");*/
-        return myMatch(0,0,s.length(),p.length(),s,p)==1;
+        return myMatch(0,0,s.length(),p.length())==1;
     }
-    int myMatch(int x, int y, int lx, int ly, string& s, string& p) {
+    int myMatch(int x, int y, int lx, int ly) {
         pair<int, int> index; index.first = x; index.second = y;
         if(dict[x][y] != 0) return dict[x][y];
         if(x == lx && y == ly) dict[x][y] = 1;
         else if(x == lx) {
             if(p[y]!='*') dict[x][y] = 2;
-            else dict[x][y] = myMatch(x, y+1, lx, ly, s, p);
+            else dict[x][y] = myMatch(x, y+1, lx, ly);
         }
         else if(y == ly) dict[x][y] = 2;
-        else if(p[y]=='?') dict[x][y] = myMatch(x+1, y+1, lx, ly, s, p);
+        else if(p[y]=='?') dict[x][y] = myMatch(x+1, y+1, lx, ly);
         else if(p[y]!='*') {
-            if(p[y]==s[x]) dict[x][y] = myMatch(x+1, y+1, lx, ly, s, p);
+            if(p[y]==s[x]) dict[x][y] = myMatch(x+1, y+1, lx, ly);
             else dict[x][y]= 2;
         }
         else {
-            if(myMatch(x, y+1, lx, ly, s, p)==1) dict[x][y] = 1;
+            if(myMatch(x, y+1, lx, ly)==1) dict[x][y] = 1;
             for(int i = 1; x+i <= s.length(); i++){
-                if(myMatch(x+i, y+1, lx, ly, s, p)==1) dict[x][y] = 1;
+                if(myMatch(x+i, y+1, lx, ly)==1) dict[x][y] = 1;
             }
         }
         if(dict[x][y]==0) dict[x][y] = 2;
