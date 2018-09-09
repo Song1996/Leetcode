7477c4099001191b5e42b3a349c4fac4eeff8ffe
Song Zhang <greatzhangs@outlook.com>
2018-09-08 17:31:30 -0700

    reimplement p229

--- p229_Majority_Element_II/p229.cpp
+++ p229_Majority_Element_II/p229.cpp
@@ -15,25 +15,25 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         vector<int> ans;
-        int c1, c2, n1, n2;
-        c1 = 0, c2 = 0, n1 = 0, n2 = 1;
+        int n1, c1, n2, c2;
+        c1 = 0, c2 = 0; n1 = -1, n2 = -2;
         for(int i = 0; i < nums.size(); i++) {
+            //printf("%d %d %d %d %d\n",nums[i], n1, c1, n2, c2);
+            if(c1==0 && nums[i]!=n2) n1 = nums[i];
+            if(c2==0 && nums[i]!=n1) n2 = nums[i];
             if(nums[i]==n1) c1++;
             else if(nums[i]==n2) c2++;
-            else if(c1==0) {
-                n1 = nums[i]; c1++;
-            } else if(c2==0) {
-                n2 = nums[i]; c2++;
-            } else {
+            else {
                 c1--; c2--;
             }
         }
-        int c = 0;
-        for(int i = 0; i < nums.size(); i++) if(nums[i]==n1) c++;
-        if(c>nums.size()/3)ans.push_back(n1);
-        c = 0;
-        for(int i = 0; i < nums.size(); i++) if(nums[i]==n2) c++;
-        if(c>nums.size()/3)ans.push_back(n2);
+        c1 = 0; c2 = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i]==n1) c1++;
+            else if(nums[i]==n2) c2++;
+        }
+        if(c1>nums.size()/3) ans.push_back(n1);
+        if(c2>nums.size()/3) ans.push_back(n2);
         return ans;
     }
 };
@@ -41,12 +41,14 @@ public:
 int main () {
     int x1[] = {3,2,3};
     vector<int> v1(x1, x1+3);
-    int x2[] = {1,1,1,3,3,2,2,2};
+    int x2[] = {1,2,2,3,2,1,1,3};
     vector<int> v2(x2, x2+8);
     Solution s;
     vector<int> y;
+    for(int i = 0; i < v1.size(); i++) printf("%d ",v1[i]); printf("\n");
     y = s.majorityElement(v1);
     for(int i = 0; i < y.size(); i++) printf("%d ",y[i]); printf("\n");
+    for(int i = 0; i < v2.size(); i++) printf("%d ",v2[i]); printf("\n");
     y = s.majorityElement(v2);
     for(int i = 0; i < y.size(); i++) printf("%d ",y[i]); printf("\n");
 	return 0;
