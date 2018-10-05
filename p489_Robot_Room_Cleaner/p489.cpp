1d5cb746350a8c66bab31094da39cc17335a064d
Song Zhang <greatzhangs@outlook.com>
2018-10-05 16:05:01 -0700

    finished p489

--- p489_Robot_Room_Cleaner/p489.cpp
+++ p489_Robot_Room_Cleaner/p489.cpp
@@ -16,6 +16,18 @@
 #include <cmath>
 using namespace std;
 
+class Robot {
+  public:
+    // Returns true if the cell in front is open and robot moves into the cell.
+    // Returns false if the cell in front is blocked and robot stays in the current cell.
+    bool move();
+    // Robot will stay in the same cell after calling turnLeft/turnRight.
+    // Each turn will be 90 degrees.
+    void turnLeft();
+    void turnRight();
+    // Clean the current cell.
+    void clean();
+};
 class Solution {
 public:
     int make_key(int x, int y) {
